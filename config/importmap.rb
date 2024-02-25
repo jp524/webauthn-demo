d9c86a314741f4db3bd29fc20b9a771805f81b98
JP <85654561+jp524@users.noreply.github.com>
2024-02-25 13:04:19 -0500

    Add github/webauthn-json and rails/request.js packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@github/webauthn-json", to: "@github--webauthn-json.js" # @2.1.1
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.9
